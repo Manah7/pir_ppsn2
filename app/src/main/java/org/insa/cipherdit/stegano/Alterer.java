652ab85290cd0d9b2dda20840dc08573042f8741
Manah7 <manah@gmx.com>
2022-04-08 16:21:16 +0200

    added template class for PPSN1

---
+++ app/src/main/java/org/insa/cipherdit/stegano/Alterer.java
@@ -0,0 +1,17 @@
+package org.insa.cipherdit.stegano;
+
+/**
+ * TODO par PPSN1
+ * Losqu'un post est trouvé, on envoie l'ID à PPSN1.
+ */
+
+public class Alterer {
+
+    public Alterer(String userId) {
+    }
+
+    public int post(String id ){
+        return 0;
+    }
+
+}
