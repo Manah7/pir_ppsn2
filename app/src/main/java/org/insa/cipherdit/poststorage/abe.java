63478fd219c761bc776eb50ba699d97b10926540
Manah7 <manah@gmx.com>
2022-04-22 15:19:57 +0200

    added interface for pir ppsn 1

---
+++ app/src/main/java/org/insa/cipherdit/poststorage/abe.java
@@ -0,0 +1,28 @@
+package org.insa.cipherdit.poststorage;
+
+import org.insa.cipherdit.reddit.things.RedditPost;
+
+import java.util.List;
+
+/**
+ * TODO par PPSN 1
+ * Permet de gérer ABE et le stockage
+ */
+
+public class abe {
+    // TODO
+    public void initAbe() {
+
+    }
+
+    // TODO Fonction appelée lors d'un envoi de posts
+    // Note : la casse des mots "id" et "ID" est respectée
+    public void postSend (RedditPost post, List<String> attributes, String id) {
+
+    }
+
+    // TODO
+    public RedditPost postGet(String ID) {
+        return null;
+    }
+}
