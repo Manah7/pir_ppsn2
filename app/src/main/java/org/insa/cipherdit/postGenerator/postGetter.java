1cf7752508321af6d92d563ace2bc167a02e8a06
NailikLN <lenezetkilian@gmail.com>
2022-04-22 14:51:39 +0200

    add post getter and fix reddit parsed post

---
+++ app/src/main/java/org/insa/cipherdit/postGenerator/postGetter.java
@@ -0,0 +1,15 @@
+package org.insa.cipherdit.postGenerator;
+
+import org.insa.cipherdit.reddit.things.RedditPost;
+
+public class postGetter {
+
+    //TODO: check if it has a hidden message and get it from dropbox (or else)
+    public static RedditPost getPostToRender(RedditPost src)
+    {
+        RedditPost newSrc;
+        newSrc = src;
+        return newSrc;
+    }
+
+}
