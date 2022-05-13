7fefd40c0e2c19f00326b53fbdeaac527a6238e8
unknown <marietou60@gmail.com>
2022-05-13 17:05:54 +0200

    conversion from RedditPost to ByteArray and vice versa OK

--- app/src/main/java/org/insa/cipherdit/cipherstorage/CipherStorage.java
+++ app/src/main/java/org/insa/cipherdit/cipherstorage/CipherStorage.java
@@ -2,7 +2,7 @@ package org.insa.cipherdit.cipherstorage;
 
 import org.insa.cipherdit.reddit.things.RedditPost;
 import java.util.List;
-
+import java.io.ByteArrayOutputStream;
 
 /**
  * TODO par PPSN 1
@@ -21,12 +21,17 @@ public class CipherStorage {
     // TODO Fonction appelée lors d'un envoi de posts
     // Note : la casse des mots "id" et "ID" est respectée
     public void postSend (RedditPost post, List<String> attributes, String id) {
-
+        byte[] postinbyte = RedditPostToByteArray(post);
+        //Encrypt
+        //store
     }
 
     // TODO
     // ID is reference to stored Object
     public RedditPost postGet(String ID) {
+
+        byte[] postinbyte = null; // what is deciphered
+        RedditPost post =  ByteArrayToRedditPost(postinbyte)
         return null;
     }
 
@@ -34,4 +39,48 @@ public class CipherStorage {
     //PRIVATE
 
 
+    //This function convert a RedditPost into a byte array
+    private byte[] RedditPostToByteArray(RedditPost post) {
+        byte[] postinbyte= null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = null;
+
+        try {
+            oos = new ObjectOutputStream(bos);
+            oos.writeObject(post);
+            oos.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        postinbyte = bos.toByteArray();
+        try {
+            bos.close();
+        } catch (IOException e) {
+            System.out.println("Error when closing the output stream.");
+        }
+
+        return postinbyte;
+    }
+
+    //This function convert a byte array into a RedditPost
+    private RedditPost ByteArrayToRedditPost(byte[] postinbyte) {
+        RedditPost post= null;
+        ByteArrayInputStream bis = new ByteArrayInputStream(postinbyte);
+        ObjectInputStream ois = null;
+
+        try {
+            ois = new ObjectInputStream(bis);
+            post = (RedditPost) ois.readObject();
+            ois.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return post;
+    }
 }
